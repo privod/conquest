725331df4d75aba863570d9d642b6b27a811d8af
privod <privod.org@gmail.com>
2021-03-15 22:04:52 +0700

    Настройки карты и локаций

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.properties import ListProperty, ObjectProperty
+from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.screenmanager import Screen
@@ -10,17 +11,44 @@ class Location(Widget):
     pass
 
 
+class Land(Location):
+    pass
+
+
+class Ocean(Location):
+    pass
+
+
 class Map(GridLayout):
     locations = ListProperty()
+
+    def build(self):
+        self.locations.append(Land())
+        self.locations.append(Land())
+        self.locations.append(Land())
+        self.locations.append(Ocean())
+        self.locations.append(Ocean())
+        self.locations.append(Ocean())
+        self.locations.append(Land())
+        self.locations.append(Land())
+
+        self.cols = 5
+        for loc in self.locations:
+            self.add_widget(loc)
 
 
 class ConquestGame(RelativeLayout):
     map = ObjectProperty(None)
 
+    def start(self):
+        self.map.build()
+
 
 class ConquestApp(App):
     def build(self):
-        return ConquestGame()
+        game = ConquestGame()
+        game.start()
+        return game
 
 
 if __name__ == '__main__':
